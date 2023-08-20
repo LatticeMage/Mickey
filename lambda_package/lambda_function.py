e607672a3a4e2733d7a7b57922af7a687cf5a99e
PosetMage <posetmage@gmail.com>
2023-08-20 12:03:43 +0800

    upload

--- lambda_package/lambda_function.py
+++ lambda_package/lambda_function.py
@@ -1,29 +1,41 @@
+
 import os
 import asyncio
 import json
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
-async def append_text_async(document_id, text_to_append):
+async def get_content_async(document_id):
     creds = Credentials.from_service_account_file('get-survey-c8bf2f366d30.json', scopes=[
         'https://www.googleapis.com/auth/documents',
         'https://www.googleapis.com/auth/drive'
     ])
     service = build('docs', 'v1', credentials=creds)
-
     document = service.documents().get(documentId=document_id).execute()
+    content = document['body']['content'][0]['paragraph']['elements'][0]['textRun']['content']
+    return content.strip()
 
-    requests = [
-        {
-            'insertText': {
-                'location': {
-                    'index': document['body']['content'][-1]['endIndex']-1,
-                },
-                'text': text_to_append
+async def set_content_async(document_id, updated_content):
+    creds = Credentials.from_service_account_file('get-survey-c8bf2f366d30.json', scopes=[
+        'https://www.googleapis.com/auth/documents',
+        'https://www.googleapis.com/auth/drive'
+    ])
+    service = build('docs', 'v1', credentials=creds)
+    requests = [{
+        'deleteContentRange': {
+            'range': {
+                'start_index': 1,
+                'end_index': len(updated_content) + 1
             }
         }
-    ]
-
+    }, {
+        'insertText': {
+            'location': {
+                'index': 1
+            },
+            'text': updated_content
+        }
+    }]
     service.documents().batchUpdate(documentId=document_id, body={'requests': requests}).execute()
 
 def lambda_handler(event, context):
@@ -31,44 +43,53 @@ def lambda_handler(event, context):
         'Access-Control-Allow-Headers': 'content-type',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,HEAD',
-        'Access-Control-Allow-Credentials' : True
+        'Access-Control-Allow-Credentials': True
     }
 
     # Preflight request. Reply successfully:
-    if event['httpMethod'] == 'OPTIONS' :
+    if event['httpMethod'] == 'OPTIONS':
         return {
             'statusCode': 200,
             'headers': headers,
             'body': 'preflight successful'
         }
 
-    # Check if 'body' is in the event and try to extract 'text' and 'document_id'
-    if 'body' not in event:
-        print("No 'body' in event")
+    # Extract document_id from the request
+    document_id = event.get('queryStringParameters', {}).get('document_id') if event['httpMethod'] == 'GET' else json.loads(event.get('body', '{}')).get('document_id')
+    if not document_id:
         return {
             'statusCode': 400,
             'headers': headers,
-            'body': json.dumps('No body provided in the request')
+            'body': json.dumps('No document_id provided in the request')
         }
-    else:
+
+    if event['httpMethod'] == 'GET':
+        loop = asyncio.get_event_loop()
+        content = loop.run_until_complete(get_content_async(document_id))
+        return {
+            'statusCode': 200,
+            'headers': headers,
+            'body': json.dumps(content)
+        }
+    
+    if event['httpMethod'] == 'POST' and 'body' in event:
         body = json.loads(event['body'])
-        if 'text' not in body or 'document_id' not in body:
-            print("No 'text' or 'document_id' in body")
+        if 'text' not in body:
             return {
                 'statusCode': 400,
                 'headers': headers,
-                'body': json.dumps('No text or document_id provided in the request body')
+                'body': json.dumps('No text provided in the request body')
             }
-        else:
-            text_to_append = body['text']
-            document_id = body['document_id']
-            loop = asyncio.get_event_loop()
-            loop.run_until_complete(append_text_async(document_id, text_to_append))
-
-            message = "Text added to Google Doc file"
-            print(message)
-            return {
-                'statusCode': 200,
-                'headers': headers,
-                'body': json.dumps(message)
-            }
+        updated_content = body['text']
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(set_content_async(document_id, updated_content))
+        return {
+            'statusCode': 200,
+            'headers': headers,
+            'body': json.dumps('Content updated in Google Doc')
+        }
+    return {
+        'statusCode': 400,
+        'headers': headers,
+        'body': json.dumps('Invalid request method')
+    }
